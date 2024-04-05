af116c7f6a8e347dddb4e0ae800710bf7ebda923
Mehdi <mehdijmlkh1382@gmail.com>
2024-04-05 09:21:48 +0330

    feat: add error message related to validation of minimum execution quantity

--- src/main/java/ir/ramtung/tinyme/messaging/Message.java
+++ src/main/java/ir/ramtung/tinyme/messaging/Message.java
@@ -4,6 +4,7 @@ public class Message {
     public static final String INVALID_ORDER_ID = "Invalid order ID";
     public static final String ORDER_QUANTITY_NOT_POSITIVE = "Order quantity is not-positive";
     public static final String ORDER_PRICE_NOT_POSITIVE = "Order price is not-positive";
+    public static final String ORDER_MINIMUM_EXECUTION_QUANTITY_NOT_POSITIVE = "Order minimum execution quantity is not-positive";
     public static final String UNKNOWN_SECURITY_ISIN = "Unknown security ISIN";
     public static final String ORDER_ID_NOT_FOUND = "Order ID not found in the order book";
     public static final String INVALID_PEAK_SIZE = "Iceberg order peak size is out of range";
@@ -13,5 +14,6 @@ public class Message {
     public static final String BUYER_HAS_NOT_ENOUGH_CREDIT = "Buyer has not enough credit";
     public static final String QUANTITY_NOT_MULTIPLE_OF_LOT_SIZE = "Quantity is not a multiple of security lot size";
     public static final String PRICE_NOT_MULTIPLE_OF_TICK_SIZE = "Price is not a multiple of security tick size";
+    public static final String MINIMUM_EXECUTION_QUANTITY_NOT_LESS_THAN_OR_EQUAL_TO_QUANTITY = "Minimum execution quantity is not less than or equal to quantity";
     public static final String SELLER_HAS_NOT_ENOUGH_POSITIONS = "Seller has not enough positions";
 }
