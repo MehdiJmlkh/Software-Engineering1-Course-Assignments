fb5ef1c9cc4870de6df7fbae3e70e1f53bc7cff5
Mehdi <mehdijmlkh1382@gmail.com>
2024-04-27 19:15:28 +0330

    style: replace redundant code with function

--- src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
+++ src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
@@ -63,11 +63,11 @@ public class Matcher {
     public MatchResult execute(Order order) {
         Order orderSnapshot = order.snapshot();
         if (order instanceof StopLimitOrder stopLimitOrder) {
-            if (order.getSide() == Side.BUY && !order.getBroker().hasEnoughCredit((long) order.getQuantity() * order.getPrice()))
+            if (order.getSide() == Side.BUY && !order.getBroker().hasEnoughCredit(order.getValue()))
                 return MatchResult.notEnoughCredit();
             if (!stopLimitOrder.isActivatable(order.getSecurity().getMarketPrice())) {
                 if (order.getSide() == Side.BUY)
-                    order.getBroker().decreaseCreditBy((long) order.getQuantity() * order.getPrice());
+                    order.getBroker().decreaseCreditBy(order.getValue());
                 order.getSecurity().getOrderBook().enqueue(order);
                 return MatchResult.notActivatable();
             }
