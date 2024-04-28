0c2086b8c667ee1d5bf5674f4ee62b7be56ad872
Mehdi <mehdijmlkh1382@gmail.com>
2024-04-28 13:21:16 +0330

    revert to my changes

--- src/test/java/ir/ramtung/tinyme/domain/OrderHandlerTest.java
+++ src/test/java/ir/ramtung/tinyme/domain/OrderHandlerTest.java
@@ -501,5 +501,4 @@ public class OrderHandlerTest {
         orderHandler.handleDeleteOrder(new DeleteOrderRq(2, "ABC", Side.BUY, 11));
         verify(eventPublisher).publish(new OrderDeletedEvent(2, 11));
     }
-
 }
